db5bc6aca574bd97b2b2c70e32a9c14bcbaecea1
JoshuaSeth <SB@accette.eu>
2019-11-04 18:43:37 +0100

    Settings Menu now contains a setting for setting the columns and rows in which PDFs will be displayed.

--- AppSettings.py
+++ AppSettings.py
@@ -1,9 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QAction, qApp, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QMainWindow, QLabel
+from PyQt5.QtWidgets import QWidget, QAction, qApp, QApplication, QVBoxLayout, QHBoxLayout, QPushButton, QMainWindow, QLabel, QGridLayout, QSpinBox
 from PyQt5.QtGui import QIcon
 
 
-class SettingsMenu(QMainWindow):
+class SettingsMenu():
+
+    documentShowColumns = 2
+    documentShowRows = 2
+
+
 
     def __init__(self):
         super().__init__()
@@ -17,38 +22,80 @@ class SettingsMenu(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        #Main layout
-
         self._new_window = NewWindow()
+        self._new_window.settingsMenu = self
         self._new_window.__init__()
+        self._new_window.load()
         self._new_window.show()
 
         print('loaded preferences')
 
 
 class NewWindow(QMainWindow):
+    settingsMenu = None
+
     def __init__(self):
         super(NewWindow, self).__init__()
+
+    def load(self):
+
+        #Main layout
         self._new_window = None
         mainWidget = QWidget(self)
         self.setCentralWidget(mainWidget)
-        mainContainer = QHBoxLayout()
-        mainWidget.setLayout(mainContainer)
+        self.mainContainer = QHBoxLayout()
+        mainWidget.setLayout(self.mainContainer)
 
         # Buttons
         buttonsContainer = QVBoxLayout()
-        mainContainer.addLayout(buttonsContainer)
+        self.mainContainer.addLayout(buttonsContainer)
 
-        self.startSearchButton = QPushButton("Page Display")
-        self.startSearchButton.setToolTip('Page Display')
-        # self.startSearchButton.clicked.connect(self.RunProgram)
-        buttonsContainer.addWidget(self.startSearchButton)
+        self.showPagesOptions = QPushButton("Page Display")
+        self.showPagesOptions.setToolTip('Page Display')
+        self.showPagesOptions.clicked.connect(self.SetOptionsContent)
+        buttonsContainer.addWidget(self.showPagesOptions)
         self.show()
+
+    def SetOptionsContent(self):
+        print('setting content')
+        #Make a grid for the labels and the spinboxes
+        fieldsContainer = QHBoxLayout()
+
+        twoFields1 = QVBoxLayout()
+        widthLabel = QLabel("Show Documents Columns")
+        self.widthInput = QSpinBox()
+        self.widthInput.setValue(self.settingsMenu.documentShowColumns)
+        self.widthInput.valueChanged.connect(self.SetPagesValues)
+        twoFields1.addWidget(widthLabel)
+        twoFields1.addWidget(self.widthInput)
+
+
+        twoFields2 = QVBoxLayout()
+        heightLabel = QLabel("Show Documents Rows")
+        self.heightInput = QSpinBox()
+        self.heightInput.setValue(self.settingsMenu.documentShowRows)
+        self.heightInput.valueChanged.connect(self.SetPagesValues)
+        twoFields2.addWidget(heightLabel)
+        twoFields2.addWidget(self.heightInput)
+
+        fieldsContainer.addLayout(twoFields1)
+        fieldsContainer.addLayout(twoFields2)
+
+        self.mainContainer.addLayout(fieldsContainer)
+
+        #add grid to maincontainer
+
+    def SetPagesValues(self):
+        self.settingsMenu.documentShowColumns = self.widthInput.value
+        self.settingsMenu.documentShowRows = self.heightInput.value
+
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    gui = NewWindow()
+    gui = SettingsMenu()
     gui.show()
     app.exec_()
+
+
 
